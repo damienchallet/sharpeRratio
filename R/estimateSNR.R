f2d9cd764f68e8faadb6e7cb0fca49a9c84a64a4
Damien Challet <damien@t450s>
2018-11-01 14:42:33 +0100

    the fit of sample values x to a Student t-distribution is now made on c(x,-x), ensuring a zero average in a nonparametric way

---
+++ R/estimateSNR.R
@@ -0,0 +1,55 @@
+library("ghyp")
+library("Rcpp")
+
+
+a=get(load("data/a_vs_R0dN.rda"))
+
+theta=function(R0dN,nu){
+  myEstimate=a(R0dN)*(1-8/3*nu^(-1.5))
+  return(myEstimate)
+}
+
+estimateSNR=function(x,numPerm=1000,nu=NA){
+
+  if(length(x)<1){
+    return(NULL)
+  }
+  x=x[is.finite(x)]
+  if(length(x)<1){
+    return(NULL)
+  }
+  x=as.numeric(x)
+  if(is.na(nu) || nu<0 ){
+    myfit=tryCatch(fit.tuv(c(x,-x),silent=TRUE,nu=6),error=function(e){return(NA)})
+
+    if(!class(myfit)=="mle.ghyp"){
+      nu=100000  #fit.tuv fails on Gaussian data. TODO: use a test to find it out
+      #    list(nu=NA,SNR=NA,R0bar=NA,N=length(x))   # could not fit at all
+    }else{
+      nu=coef(myfit)$nu
+    }
+  }
+
+  R0bar=computeR0bar(x,numPerm = numPerm)   #computes R_+ - R_- over numPerm permutations
+
+  if(abs(R0bar)<1){
+    R0bar=0
+    return(list(nu=nu,SNR=0,R0bar=0,N=length(x)))
+  }
+
+  N=length(x)   # note: this is the length of x[is.finite(x)], which removes NAs and Inf
+  SNR_raw=theta(abs(R0bar)/length(x),nu)                #splines have been calibrated for positive SNRs, hence abs(R0bar)
+  SNR_raw=SNR_raw * (sign(SNR_raw)>0)                   #if(spline(abs(x)))<0, then set SNR to 0 (this may occur when SNR is very small)
+  SNR=SNR_raw*sign(R0bar)                               #then apply sign of R0bar
+
+  if(SNR==0){
+    browser()
+  }
+
+
+  return(list(nu=nu,SNR=SNR,R0bar=R0bar,N=length(x)))
+}
+
+.onUnload <- function (libpath) {
+  library.dynam.unload("sharpeRratio", libpath)
+}
