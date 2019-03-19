c066aab3dc2feee2742fba941235dd36ae158067
Damien Challet <damien.challet@gmail.com>
2019-03-19 21:10:06 +0100

    improved splines

--- R/estimateSNR.R
+++ R/estimateSNR.R
@@ -2,10 +2,16 @@ library("ghyp")
 library("Rcpp")
 
 
-a=get(load("data/a_vs_R0dN.rda"))
+a=get(load("data/a.rda"))
+f=get(load("data/f.rda"))
 
-theta=function(R0dN,nu){
-  myEstimate=a(R0dN)*(1-8/3*nu^(-1.5))
+b=function(r0,N){
+  r0Nalpha=r0*N^.42
+  return(exp(f(r0Nalpha))*r0^1.6)
+}
+
+theta=function(r0,N,nu){
+  myEstimate=a(r0)-b(r0,N)*nu^(-1.5)
   return(myEstimate)
 }
 
@@ -15,7 +21,8 @@ estimateSNR=function(x,numPerm=1000,nu=NA){
     return(NULL)
   }
   x=x[is.finite(x)]
-  if(length(x)<1){
+  N=length(x)
+  if(N<1){
     return(NULL)
   }
   x=as.numeric(x)
@@ -24,7 +31,6 @@ estimateSNR=function(x,numPerm=1000,nu=NA){
 
     if(!class(myfit)=="mle.ghyp"){
       nu=100000  #fit.tuv fails on Gaussian data. TODO: use a test to find it out
-      #    list(nu=NA,SNR=NA,R0bar=NA,N=length(x))   # could not fit at all
     }else{
       nu=coef(myfit)$nu
     }
@@ -38,13 +44,10 @@ estimateSNR=function(x,numPerm=1000,nu=NA){
   }
 
   N=length(x)   # note: this is the length of x[is.finite(x)], which removes NAs and Inf
-  SNR_raw=theta(abs(R0bar)/length(x),nu)                #splines have been calibrated for positive SNRs, hence abs(R0bar)
+  SNR_raw=theta(abs(R0bar)/N,N,nu)                #splines have been calibrated for positive SNRs, hence abs(R0bar)
+
   SNR_raw=SNR_raw * (sign(SNR_raw)>0)                   #if(spline(abs(x)))<0, then set SNR to 0 (this may occur when SNR is very small)
   SNR=SNR_raw*sign(R0bar)                               #then apply sign of R0bar
-
-  if(SNR==0){
-    browser()
-  }
 
 
   return(list(nu=nu,SNR=SNR,R0bar=R0bar,N=length(x)))
